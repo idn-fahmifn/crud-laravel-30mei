dfefb1729660447639ea9eb8670343161de39236
idn-fahmifn <fahmifn.idn@gmail.com>
2026-05-31 15:39:16 +0700

    4.15

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -6,6 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Item extends Model
 {
-    
+    protected $fillable = [
+        'uuid', 'item_name', 'location_id', 'category', 'status', 'desc'
+    ];
+
+    public function location()
+    {
+        return $this->belongsTo(Location::class, 'location_id');
+    }
 
 }
